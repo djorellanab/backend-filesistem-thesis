2123754ffaa5638dfdae0191bae155e1cc0d72e7
Diego Orellana <djorellanab@correo.url.edu.gt>
2019-06-26 16:17:14 -0600

    fix schema

--- schemas/functional-movement.js
+++ schemas/functional-movement.js
@@ -2,28 +2,61 @@ const moongose = require('mongoose');
 
 let Schema = moongose.Schema;
 
-let stepFunctionalMovement = new Schema({
-    step: {
-        type: Number,
-        required: [true, 'El numero del paso se requiere']
+let functionalMovement = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Nombre del movimiento funcional es necesario']
     },
-    time: {
-        type: Number,
-        required: [true, 'El tiempo de toma de datos se requiere']
+    description: {
+        type: String,
+        required: [true, 'La descripcion es necesario']
     },
-    factorMovement: {
+    steps:[{
         type: Number,
-        required: [true, 'El factor de movimiento de los datos se requiere']
-    },
-    clasification:{
+        required: [true, 'Es necesario definir los pasos del movimiento funcional'] 
+    }],
+    anglesOfMovement:[{
+        type: Number,
+        required: [true, 'Es necesario definir los angulos del movimiento funcional'] 
+    }],
+    movementFactor: {
+        type: Number,
+        default: 0.1
+    }, 
+    height : {
+        type: Number,
+        required: [true, 'La altura del kinect del suelo'] 
+    }, 
+    depthMin : {
+        type: Number,
+        required: [true, 'La profundidad minima del kinect del suelo'] 
+    }, 
+    depthMax : {
+        type: Number,
+        required: [true, 'La profundidad maxima del kinect del suelo'] 
+    }, 
+    time_stamp : {
+        type: Number,
+        required: [true, 'Fecha de insercion es necesaria'] 
+    }, 
+    state : {
         type: Boolean,
-        required: [true, 'La clasificacion del paso es requerida'] 
-    },
-    detailsOfStepFunctionalMovement:[ {
+        default: true
+    }, 
+    focusJoin  : {
+        type: Number,
+        required: [true, 'centro de analisis es necesario'] 
+    }, 
+    file: {
         type: Schema.Types.ObjectId,
-        ref: 'DetailOfStepFunctionalMovement',
+        ref: 'File',
+        required: false
+    },
+    stepsFunctionalMovement:[ {
+        type: Schema.Types.ObjectId,
+        ref: 'StepFunctionalMovement',
         required: false
     }]
 });
 
-module.exports = moongose.model('StepFunctionalMovement', stepFunctionalMovement);
+module.exports = moongose.model('FunctionalMovement', functionalMovement);
